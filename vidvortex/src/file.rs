5f44eacec1e4c5388289473a9eb1126643bc5985
jiisanda <jiisanda@protonmail.com>
2024-01-20 00:04:20 +0530

    📽️ file size

--- vidvortex/src/file.rs
+++ vidvortex/src/file.rs
@@ -1,9 +1,10 @@
+use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
 use mime_guess::from_path;
 
 
-pub fn get_video_files(dir: &Path) -> Vec<String> {
+pub fn get_video_files(dir: &Path) -> Vec<(String, u64)> {
     let vec = WalkDir::new(dir)
         .into_iter()                    // converts walkdir instance to iterator
         .filter_map(Result::ok)           // filter: entries that could be read without errors
@@ -12,7 +13,12 @@ pub fn get_video_files(dir: &Path) -> Vec<String> {
             let mime = from_path(e.path()).first_raw().unwrap_or("");
             mime.starts_with("video/")
         })
-        .map(|e| e.path().to_string_lossy().into_owned())
+        .filter_map(|e| {
+            let path = e.path();
+            let metadata = fs::metadata(path).ok()?;
+            let size = metadata.len();
+            Some((path.to_string_lossy().into_owned(), size))
+        })
         .collect();
     vec
 }
--- vidvortex/src/main.rs
+++ vidvortex/src/main.rs
@@ -3,6 +3,6 @@ mod file;
 fn main() {
     let video_files = file::get_video_files(std::path::Path::new("."));
     for file in video_files {
-        println!("{}", file);
+        println!("{:?}", file);
     }
 }
