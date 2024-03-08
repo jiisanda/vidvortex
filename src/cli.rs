fd3adbbc325ff1375f4eb145f6812eaedf32f317
jiisanda <jiisanda@protonmail.com>
2024-03-08 15:47:32 +0530

    cli.rs - handle args parse

---
+++ src/cli.rs
@@ -0,0 +1,28 @@
+use getopts::Options;
+use std::{env, process};
+
+pub fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+pub fn parse_args() -> (String, Options, getopts::Matches) {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "list of all available arguments");
+    opts.optflag("l", "list", "list video files in the current directory and subdirectories");
+    opts.optopt("p", "path", "set the path to search for video files", "PATH");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("{}", e);
+            print_usage(&program, opts);
+            process::exit(1);
+        }
+    };
+
+    (program, opts, matches)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,22 @@
-use getopts::Options;
-
-use std::env;
 use std::path::Path;
 
+mod cli;
 mod file;
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("{}", opts.usage(&brief));
-}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "list of all available arguments");
-    opts.optflag("l", "list", "list video files in the current directory and subdirectories");
-
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(e) => {
-            eprintln!("{}", e);
-            print_usage(&program, opts);
-            return;
-        }
-    };
+    let (program, opts, matches) = cli::parse_args();
 
     if matches.opt_present("h") {
-        print_usage(&program, opts);
+        cli::print_usage(&program, opts);
         return;
     }
 
+    let path = matches.opt_str("p").unwrap_or_else(|| ".".to_string());
+
     if matches.opt_present("l") {
-        let video_files = file::get_video_files(Path::new("."));
+        println!("path: {:?}", &path);
+        let video_files = file::get_video_files(Path::new(&path));
         for file in video_files {
             println!("{:?}", file);
         }
