958b6e4b7cdf6244a217aa8bafb7f9e008858f8b
jiisanda <jiisanda@protonmail.com>
2024-03-08 18:28:40 +0530

    get video duration

--- src/file.rs
+++ src/file.rs
@@ -1,10 +1,39 @@
+use std::error::Error;
 use std::fs;
 use std::path::Path;
+use std::process::Command;
+use std::str;
 use walkdir::WalkDir;
 use mime_guess::from_path;
 
+pub fn get_video_length(file_path: &Path) -> Result<(f64, String), Box<dyn Error>> {
+    let output = Command::new("ffmpeg")
+        .arg("-i")
+        .arg(file_path.to_str().ok_or("Failed to convert path to string")?)
+        .output()?;
 
-pub fn get_video_files(dir: &Path) -> Vec<(String, String)> {
+    let output_str = str::from_utf8(&output.stderr);
+
+    let duration_line = output_str.unwrap().lines()
+        .find(|line| line.contains("Duration"))
+        .ok_or("Failed to find duration in output")?;
+
+    let duration_str: String = duration_line.split("Duration: ").nth(1)
+        .and_then(|s| s.split(',').next())
+        .ok_or("Failed to parse duration from output")?
+        .to_string();
+
+    let time_parts: Vec<&str> = duration_str.split(":").collect();
+    let hrs: f64 = time_parts[0].parse()?;
+    let min: f64 = time_parts[1].parse()?;
+    let sec: f64 = time_parts[2].parse()?;
+
+    let duration = hrs * 3600.0 + min * 60.0 + sec;
+
+    Ok((duration, duration_str))
+}
+
+pub fn get_video_files(dir: &Path) -> Vec<(String, String, Result<(f64, String), Box<dyn Error>>)> {
     let vec = WalkDir::new(dir)
         .into_iter()                    // converts walkdir instance to iterator
         .filter_map(Result::ok)           // filter: entries that could be read without errors
@@ -17,7 +46,8 @@ pub fn get_video_files(dir: &Path) -> Vec<(String, String)> {
             let path = e.path();
             let metadata = fs::metadata(path).ok()?;
             let size_mb = metadata.len() as f64 / 1_000_000.0;
-            Some((path.to_string_lossy().into_owned(), format!("{:.2} MB", size_mb)))
+            let duration = get_video_length(path);
+            Some((path.to_string_lossy().into_owned(), format!("{:.2} MB", size_mb), duration))
         })
         .collect();
     vec
