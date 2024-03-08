dfe057aa7f712d0d2c047f550889fa88fb572ba5
jiisanda <jiisanda@protonmail.com>
2024-03-08 16:04:59 +0530

    -l size in MB

--- src/file.rs
+++ src/file.rs
@@ -4,7 +4,7 @@ use walkdir::WalkDir;
 use mime_guess::from_path;
 
 
-pub fn get_video_files(dir: &Path) -> Vec<(String, u64)> {
+pub fn get_video_files(dir: &Path) -> Vec<(String, String)> {
     let vec = WalkDir::new(dir)
         .into_iter()                    // converts walkdir instance to iterator
         .filter_map(Result::ok)           // filter: entries that could be read without errors
@@ -16,8 +16,8 @@ pub fn get_video_files(dir: &Path) -> Vec<(String, u64)> {
         .filter_map(|e| {
             let path = e.path();
             let metadata = fs::metadata(path).ok()?;
-            let size = metadata.len();
-            Some((path.to_string_lossy().into_owned(), size))
+            let size_mb = metadata.len() as f64 / 1_000_000.0;
+            Some((path.to_string_lossy().into_owned(), format!("{:.2} MB", size_mb)))
         })
         .collect();
     vec
